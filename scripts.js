b06b4bea6d9d0595dd3ac8e3d40ae6c93ffa1dbd
Robert Minshull <robminshull126@gmail.com>
2022-12-03 13:50:08 +0000

    Buttons amended to remove text and add sample emojis, functionality also added via js file

---
+++ scripts.js
@@ -0,0 +1,15 @@
+function copyText1(){
+    navigator.clipboard.writeText("😀");
+}
+function copyText2(){
+    navigator.clipboard.writeText("😃");
+}
+function copyText3(){
+    navigator.clipboard.writeText("😄");
+}
+function copyText4(){
+    navigator.clipboard.writeText("😁");
+}
+function copyText5(){
+    navigator.clipboard.writeText("😆");
+}
