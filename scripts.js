b94a995e2df007daeb2317c97da7ad3729fd8c8e
Robert Minshull <robminshull126@gmail.com>
2022-12-04 22:14:29 +0000

    JS function updated as per chatGPT suggestion

--- scripts.js
+++ scripts.js
@@ -1,15 +1,24 @@
-function copyText1(){
-    navigator.clipboard.writeText("😀");
-}
-function copyText2(){
-    navigator.clipboard.writeText("😃");
-}
-function copyText3(){
-    navigator.clipboard.writeText("😄");
-}
-function copyText4(){
-    navigator.clipboard.writeText("😁");
-}
-function copyText5(){
-    navigator.clipboard.writeText("😆");
-}
+function writeToClipboard(text) {
+    // Create a hidden textarea element
+    var textarea = document.createElement("textarea");
+  
+    // Set the textarea to be invisible
+    textarea.style.position = "absolute";
+    textarea.style.left = "-9999px";
+    textarea.style.top = "0";
+  
+    // Set the text to write to the clipboard
+    textarea.value = text;
+  
+    // Add the textarea to the page
+    document.body.appendChild(textarea);
+  
+    // Select the text in the textarea
+    textarea.select();
+  
+    // Copy the selected text to the clipboard
+    document.execCommand("copy");
+  
+    // Remove the textarea from the page
+    document.body.removeChild(textarea);
+  }
